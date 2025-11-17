9c0fa22fc931d3dffbeb5021f4d79b9535db4176
44fahadhasan <44fahadhasan@gmail.com>
2025-11-17 09:36:03 +0600

    person class added

--- solution.ts
+++ solution.ts
@@ -12,3 +12,20 @@ function getLength(value: GetLengthT): number | undefined {
   if (typeof value === "string") return value.length;
   if (Array.isArray(value)) return value.length;
 }
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `'Name: ${[this.name]}, Age: ${[this.age]}'`;
+  }
+}
+
+const person1 = new Person("John Doe", 30);
+const person2 = new Person("Alice", 25);
