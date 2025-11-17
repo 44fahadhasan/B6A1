8b757e7d3cce2c57a5656bdc8d12c814a630e1a9
44fahadhasan <44fahadhasan@gmail.com>
2025-11-17 09:26:59 +0600

    get length fun added

--- solution.ts
+++ solution.ts
@@ -5,3 +5,10 @@ function formatValue(value: FormatValueT): FormatValueT | undefined {
   if (typeof value === "number") return value * 10;
   if (typeof value === "boolean") return !value;
 }
+
+type GetLengthT = string | number[];
+
+function getLength(value: GetLengthT): number | undefined {
+  if (typeof value === "string") return value.length;
+  if (Array.isArray(value)) return value.length;
+}
